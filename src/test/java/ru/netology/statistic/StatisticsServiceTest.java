cd68aefaf13a23b74724e8391498a91bba984a81
SizovaQA88 <nataliasiz19@bk.ru>
2024-08-01 13:07:17 +1000

    second test

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -6,11 +6,23 @@ import org.junit.jupiter.api.Test;
 public class StatisticsServiceTest {
 
     @Test
-    void findMax() {
+    public void findMax() {
         StatisticsService service = new StatisticsService();
 
         long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
         long expected = 12;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void findMaxBetweenThem() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 14};
+        long expected = 14;
 
         long actual = service.findMax(incomesInBillions);
 
